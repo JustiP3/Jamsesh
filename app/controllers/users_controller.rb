20bf00275071baaa79f99b943a85c007cc198d4b
justin <justinphil02@yahoo.com>
2020-04-08 19:37:01 -0700

    frame user controller and create templates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+    before action 
+    before_action :find_user, only: [:edit, :show, :update, :destroy]
+    def index
+        @users = User.all 
+    end 
+    def create 
+    end 
+    def new
+        @user = User.new 
+    end 
+    def edit
+    end 
+    def show 
+    end 
+    def update
+    end 
+    def destroy
+    end 
+
+    private
+
+    def user_params
+        params.require(:users).permit(:name, :password, :instrument)
+    end 
+
+    def find_user
+        @user = User.find_by(id: params[:id])
+    end 
 end
