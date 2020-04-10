ab303a09f512e9c6c9be43970eb0cce83778f2b7
Justin <justinphil02@yahoo.com>
2020-04-09 20:18:22 -0700

    fix sessions controller, create login template

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,27 @@
 class SessionsController < ApplicationController
-  def login
+
+  def new 
+    @user = User.new    
+    render :login
   end
-  def signup
-  end 
+
+  def create
+    @user = User.find_by(username: params[:username]) 
+    if user.authenticate(@user)
+      session[:username] = @user.username 
+      redirect_to user_path(@user)
+    else
+      render :login 
+    end 
+  end
 
   def destroy
+    session.delete :username
   end
+
+  private
+
+  def session_params
+    params.require(:sessions).permit(:username, :password)
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'sessions#signup'
-  get 'login', to: 'sessions#login'
-  destroy '/sessions/:id', to: 'sessions#destroy'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :edit, :update, :create, :show, :index]
   
