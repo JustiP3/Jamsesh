e1f37202f7cd6d908322477a9ae7ee0958d5a4ed
Justin <justinphil02@yahoo.com>
2020-04-09 20:28:40 -0700

    add user errors helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: params[:username]) 
-    if user.authenticate(@user)
+    if user.authenticate(params[:password])
       session[:username] = @user.username 
       redirect_to user_path(@user)
     else
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+    
+    def user_errors(@user)
+        if @user.errors.any? 
+            "Errors:"
+            @user.errors.full_messages.each do |msg|
+                msg
+            end
+        end 
+    end 
 end
