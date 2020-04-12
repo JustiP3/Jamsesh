88b8fefcf0ba3508c854634119d2d9dbe7896460
Justin <justinphil02@yahoo.com>
2020-04-11 21:05:55 -0700

    login displays error messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,6 +12,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id 
       redirect_to user_path(@user)
     else
+      @error = "Sorry, the password was incorrect." if @user
       render :login 
     end 
   end
