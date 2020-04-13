b8d3ca257306c5a97e6243eb61d769a74455fe43
Justin <justinphil02@yahoo.com>
2020-04-12 18:43:26 -0700

    remove title from comments table

---
+++ db/migrate/20200413014229_remove_title_from_comments.rb
@@ -0,0 +1,6 @@
+class RemoveTitleFromComments < ActiveRecord::Migration[6.0]
+  def change
+
+    remove_column :comments, :title, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_012228) do
+ActiveRecord::Schema.define(version: 2020_04_13_014229) do
 
   create_table "bands", force: :cascade do |t|
     t.string "name"
@@ -28,7 +28,6 @@ ActiveRecord::Schema.define(version: 2020_04_13_012228) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string "title"
     t.text "content"
     t.integer "user_id"
     t.integer "post_id"
