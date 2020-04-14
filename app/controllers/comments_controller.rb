69d6e53a1c8ca23bc222de6bc63ba202e09579fa
Justin <justinphil02@yahoo.com>
2020-04-13 19:02:34 -0700

    add find post and comment params to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,24 @@
 class CommentsController < ApplicationController
+    before_action :find_post, only: [:create, :new, :index, :destroy]
+    def new 
+        @comment = Comment.new
+    end 
+    def create        
+    
+    end 
+    def index 
+    end 
+    
+    def destroy 
+    end 
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content, :user_id, :post_id)
+    end 
+
+    def find_post
+        @post = Post.find_by(id: params[:post_id])
+    end 
 end
