b3866fd4d9f44a91357991477c75c9dd15eca323
Justin <justinphil02@yahoo.com>
2020-04-13 19:16:08 -0700

    add create method, add comment validations.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,16 @@
 class CommentsController < ApplicationController
-    before_action :find_post, only: [:create, :new, :index, :destroy]
+    before_action :find_post
     def new 
         @comment = Comment.new
     end 
     def create        
-    
+        @comment = Comment.new(comment_params)
+        if @comment.save 
+            redirect_to post_comments_path(@post, @comment)
+        else 
+            flash[:message] = "A comment cannot be blank"
+            redirect_to new_post_comment_path(@post, @comment)
+        end 
     end 
     def index 
     end 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :post 
     accepts_nested_attributes_for :posts
+    validates :post_id, presence: true 
 end
