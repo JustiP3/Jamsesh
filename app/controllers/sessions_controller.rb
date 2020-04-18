8d2b291ba7de587c59dd3505c85791baff4c7fa9
Justin <justinphil02@yahoo.com>
2020-04-18 14:01:58 -0700

    refactor sessions login to make controller look nice

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,20 +13,15 @@ class SessionsController < ApplicationController
     if (p_user && p_user.authenticate(params[:password])) || o_user   #good login 
       p_user ||= o_user 
       session[:user_id] = p_user.id 
-      redirect_to user_path(p_user)          
-    elsif p_user       #traditional login attempt but pw not authenticated 
-      @error = "Sorry, the password was incorrect."
-      render :login 
-    elsif request.env["omniauth.auth"] && !request.env["omniauth.auth"]["info"]["email"]  #omni login but email nil
-      @error = "Please set your github email to publically viewable in your github account settings."
-      render :login
-    elsif request.env["omniauth.auth"]["info"]["email"] #omni login attempt doesn't match any existing users
-      flash["message"] = "You must create an account before you can log in with github."
-      redirect_to new_user_path
-    else 
-      redirect_to root_path 
-    end      
+      redirect_to user_path(p_user)      
+    else           
+      find_error(o_user, p_user, request.env["omniauth.auth"])
+    end 
   end 
+
+  
+
+  # dont forget to add this in there 
 
   def destroy
     session.delete :user_id
@@ -46,4 +41,26 @@ class SessionsController < ApplicationController
   def user_by_params
     User.find_by(session_params) if params[:sessions]
   end 
+
+  def find_error (o_user, p_user, omni_hash)
+    if  p_user      #traditional login attempt but pw not authenticated 
+      @error = "Sorry, the password was incorrect."
+      render :login   
+    
+    elsif request.env["omniauth.auth"] #omni login 
+      if !request.env["omniauth.auth"]["info"]["email"]  #omni login but email nil
+        @error = "Please set your github email to publically viewable in your github account settings."
+        render :login
+      elsif request.env["omniauth.auth"]["info"]["email"] #omni login attempt doesn't match any existing users
+        flash["message"] = "You must create an account before you can log in with github."
+        redirect_to new_user_path
+      end 
+      
+    elsif !p_user && !o_user   # if not any above errors, user tried to log in traditionally but user wasn't found 
+      @error = "Sorry we could not find that user"  
+    else   # catch all to not crash my app. should never execute 
+      redirect_to root_path 
+    end   
+  end    
+
 end
