d7205d44aba6442577195b94ef6310e85ac95b8b
Justin <justinphil02@yahoo.com>
2020-04-19 12:27:43 -0700

    add post helpers to make post show cleaner

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,9 @@
 module PostsHelper
+    def post_by
+        "post by " + User.find(@post.author_id).username
+    end 
+
+    def back_to_the_band
+        "Back to the band - " + @post.band.name
+    end 
 end
