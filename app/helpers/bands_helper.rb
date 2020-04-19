5ceebde6686798770ed0e49adfd4cff346f6ecee
Justin <justinphil02@yahoo.com>
2020-04-19 11:11:23 -0700

    band show page displays members primary instrument

--- app/helpers/bands_helper.rb
+++ app/helpers/bands_helper.rb
@@ -1,2 +1,14 @@
 module BandsHelper
+    def band_location
+        <p><%= "Band Location: " + @band.location if @band.location %></p>
+
+    <% if @band.tags.size > 0  %>
+        <p>Tags:</p>
+        
+        <% @band.tags.each do |tag| %>
+            <p> - <%= tag.name %></p>
+        <% end %>
+        
+    <% end %>
+    end 
 end
